52d861c3a088acce1312a629fd809c6b4aec641d
mkenyah <kirikajoseph16@gmail.com>
2023-09-01 00:44:22 +0300

    :fire added a todolist

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,10 +1,18 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, {useState} from 'react'
 
 const TodoForm = () => {
+  const [value, setValue] = useState("")
+  const handleSubmit = e => {
+e.preventDefault();
+console.log(value);
+  }
   return (
-    <form className='TodoForm'>
-        <input type='text' className='todo-input' placeholder='What is the task today?'/>
+    
+  
+    <form className='TodoForm' onSubmit={handleSubmit}>
+       
+        <input type='text' className='todo-input' placeholder='What is the task today?' onChange={(e) => setValue(e.target.value)}/>
         <button  type ="submit" className='todo-btn'>Add task</button>
     </form>
   )
