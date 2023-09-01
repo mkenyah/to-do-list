1ed6af9dec480f1bd145c172545428a0cfccd4c6
mkenyah <kirikajoseph16@gmail.com>
2023-09-01 14:12:01 +0300

    todolist

---
+++ src/components/Todo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <div className='Todo'>
+        <p>go to school</p>
+    </div>
+  )
+}
+
+export default Todo
--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -1,14 +1,23 @@
+// // eslint-disable-next-line no-unused-vars
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, { useState } from 'react';
 import TodoForm from './TodoForm';
-
+import { v4 as uuidv4 } from 'uuid';
+uuidv4()
 
 const TodoWrapper = () => {
-    return(
-        <div className='TodoWrapper'>
-           <TodoForm/> 
-        </div>
-    )
-}
+  const [todos, setTodos] = useState([]); // Use square brackets here to initialize the state array
 
-export default TodoWrapper
+  const addTodo = todo => {
+    setTodos([...todos, { id: uuidv4(), task: todo, completed: false, isEditing: false }]);
+    console.log(todos);
+  };
+
+  return (
+    <div className='TodoWrapper'>
+      <TodoForm addTodo={addTodo} />
+    </div>
+  );
+};
+
+export default TodoWrapper;
